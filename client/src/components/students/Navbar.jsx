e3e7d6588da49aa7012b71e0652f0bd9ebd16154
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-07-23 20:54:56 +0530

    setup user authentication

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -12,12 +12,19 @@ const Navbar = () => {
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
           <div className='flex items-center gap-5'>
             <button>Become Educator</button>
-            <Link to='my-enrollments'>My Enrollments</Link>
+            |<Link to='my-enrollments'>My Enrollments</Link>
           </div>
           <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
         </div>
         
-        <div className='md:hidden'></div>
+        {/* For Phone Screens */}
+        <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+          <div>
+            <button>Become Educator</button>
+            |<Link to='/my-enrollments'>My Enrollments</Link>
+          </div>
+          <button><img src={assets.user_icon} alt="" /></button>
+        </div>
     </div>
   )
 }
