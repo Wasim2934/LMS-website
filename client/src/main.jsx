030bf4e09e95b51e67473fa133ae37e120c5c08b
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-07-24 21:13:24 +0530

    setup clerk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { AppContextProvider } from "./context/AppContext.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from '@clerk/react'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AppContextProvider>
+    <ClerkProvider>
       <App />
-    </AppContextProvider>
+    </ClerkProvider>
   </BrowserRouter>,
-);
+)
