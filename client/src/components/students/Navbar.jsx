20055814d084e114c35fe7fb92057f65ecb05007
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-07-26 22:55:19 +0530

    formatted code

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-300 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
-        <img src={assets.logo} alt='logo' className='w-28 lg:w-32 cursor-pointer'/>
-        <div className='hidden md:flex items-center gap-5 text-gray-500'>
-          <div className='flex items-center gap-5'>
+        <img src={assets.logo} alt='logo' className='w-28  lg:w-32  cursor-pointer'/>
+        <div className='hidden md:flex items-center  gap-5 text-gray-500'>
+          <div className='flex items-center gap-5 '>
             <button>Become Educator</button>
             |<Link to='my-enrollments'>My Enrollments</Link>
           </div>
