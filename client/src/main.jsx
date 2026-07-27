92d378a30310a77bc4a75236dc2f65593e62ebd8
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-07-27 23:27:43 +0530

    create account feature

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
-
+ 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
